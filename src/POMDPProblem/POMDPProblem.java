6b8de6c6639705e4fc5cfc200f681eae1011a6e4
Daniel Šmolík <dansmoliik@gmail.com>
2018-08-03 11:06:12 +0200

    Added functions to transform data from POMDP file format to format used in HSVI paper

--- src/POMDPProblem/POMDPProblem.java
+++ src/POMDPProblem/POMDPProblem.java
@@ -3,26 +3,28 @@ package POMDPProblem;
 import java.util.*;
 
 public class POMDPProblem {
+    // TODO rewards matrix dimension and arguments (pomdp file vs hsvi)
     final ArrayList<String> stateNames;
     final HashMap<String, Integer> stateNameToIndex;
     final ArrayList<String> actionNames;
     final HashMap<String, Integer> actionNameToIndex;
-    final double[][][] actionProbabilities; // probability p of getting from start-state ss to end-state es ...
-    //                                         p = actionProbabilities[a][ss][es]
+    final double[][][] actionProbabilities; // probability p of getting from start-state s_ to end-state s_ playing
+    //                                         action a ... p = actionProbabilities[s][a][s_]
     final ArrayList<String> observationNames;
     final HashMap<String, Integer> observationNameToIndex;
     final double[][][] observationProbabilities; // probability p of seeing an observation o playing action a getting to
-    //                                              end-state s ... p = observationProbabilities[a][s][o]
-    final double[][][][] rewards; // reward r for
+    //                                              end-state s_ ... p = observationProbabilities[s_][a][o]
+    final double[][] rewards; // reward r for playing action a in state s ... r = rewards[s][a]
     final double discount;
     final double[] initBelief;
     final boolean minimize;
 
     public POMDPProblem(List<String> stateNames, HashMap<String, Integer> stateNameToIndex,
                         List<String> actionNames, HashMap<String, Integer> actionNameToIndex,
-                        double[][][] actionProbabilities,
+                        double[][][] actionProbabilities, // ap[a][s][s_]
                         List<String> observationNames, HashMap<String, Integer> observationNameToIndex,
-                        double[][][] observationProbabilities,
+                        double[][][] observationProbabilities, //op[a][s_][o]
+                        double[][][][] rewards, //r[a][s][s_][o]
                         double discount,
                         boolean minimize,
                         double[] initBelief) {
@@ -30,10 +32,11 @@ public class POMDPProblem {
         this.stateNameToIndex = stateNameToIndex;
         this.actionNames = new ArrayList<>(actionNames);
         this.actionNameToIndex = actionNameToIndex;
-        this.actionProbabilities = actionProbabilities;
+        this.actionProbabilities = transformActionProbabilitiesToHSVI(actionProbabilities);
         this.observationNames = new ArrayList<>(observationNames);
         this.observationNameToIndex = observationNameToIndex;
-        this.observationProbabilities = observationProbabilities;
+        this.observationProbabilities = transformObservationProbabilitiesToHSVI(observationProbabilities);
+        this.rewards = transformRewardsToHSVI(rewards);
         this.discount = discount;
         this.minimize = minimize;
         this.initBelief = initBelief;
@@ -45,6 +48,7 @@ public class POMDPProblem {
                         double[][][] actionProbabilities,
                         List<String> observationNames, HashMap<String, Integer> observationNameToIndex,
                         double[][][] observationProbabilities,
+                        double[][][][] rewards,
                         double discount,
                         boolean minimize) {
         this(stateNames, stateNameToIndex,
@@ -52,6 +56,7 @@ public class POMDPProblem {
                 actionProbabilities,
                 observationNames, observationNameToIndex,
                 observationProbabilities,
+                rewards,
                 discount,
                 minimize,
                 null);
@@ -67,5 +72,52 @@ public class POMDPProblem {
             probSum += observationProbabilities[actionI][endStateI][observationI];
         }
         return probSum;
+    }
+
+    private double[][][] transformActionProbabilitiesToHSVI(double[][][] actionProbabilities) {
+        int statesCount = actionProbabilities[0].length;
+        int actionsCount = actionProbabilities.length;
+        double[][][] actionProbabilitiesTransformed = new double[statesCount][actionsCount][statesCount];
+        for (int a = 0; a < actionsCount; ++a) {
+            for (int s = 0; s < statesCount; ++s) {
+                for (int s_ = 0; s_ < statesCount; ++s_) {
+                    actionProbabilitiesTransformed[s][a][s_] = actionProbabilities[a][s][s_];
+                }
+            }
+        }
+        return actionProbabilitiesTransformed;
+    }
+
+    private double[][][] transformObservationProbabilitiesToHSVI(double[][][] observationProbabilities) {
+        int actionsCount = observationProbabilities[0].length;
+        int statesCount = observationProbabilities.length;
+        int observationsCount = observationProbabilities[0][0].length;
+        double[][][] observationProbabilitiesTransformed = new double[actionsCount][statesCount][observationsCount];
+        for (int a = 0; a < actionsCount; ++a) {
+            for (int s = 0; s < statesCount; ++s) {
+                for (int o = 0; o < statesCount; ++o) {
+                    observationProbabilitiesTransformed[s][a][o] = observationProbabilities[a][s][o];
+                }
+            }
+        }
+        return observationProbabilitiesTransformed;
+    }
+
+    private double[][] transformRewardsToHSVI(double[][][][] rewards) {
+        int statesCount = rewards[0].length;
+        int actionsCount = rewards.length;
+        int observationsCount = rewards[0][0][0].length;
+        double[][] rewardsTransformed = new double[statesCount][actionsCount];
+        for (int a = 0; a < actionsCount; ++a) {
+            for (int s = 0; s < statesCount; ++s) {
+                for (int s_ = 0; s_ < statesCount; ++s_) {
+                    for (int o = 0; o < observationsCount; ++o) {
+                        rewardsTransformed[s][a] += actionProbabilities[s][a][s_] * observationProbabilities[s_][a][o] *
+                                rewards[a][s][s_][o];
+                    }
+                }
+            }
+        }
+        return rewardsTransformed;
     }
 }
