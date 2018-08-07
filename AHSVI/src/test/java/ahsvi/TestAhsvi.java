e39085746f714af1aa919c252e8cbe17965bfbbb
Karel Durkota <karel.durkota@gmail.com>
2018-08-07 11:39:04 +0200

    initial commit for testovani

---
+++ AHSVI/src/test/java/ahsvi/TestAhsvi.java
@@ -0,0 +1,19 @@
+package ahsvi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+class AHSVITest {
+
+	@Test
+	public void test1(){
+		assertEquals("ahoj", "ahoj", "ahoj made");
+	}
+
+	@Test
+	public void test2(){
+		assertEquals("ahoj2", "ahoj", "This ahoj should be false.");
+	}
+
+}
